bb0dfa0b799929851fa7db08e2927781dbd58d08
AzvedoGabriel <contato.gabrielazevedo0@gmail.com>
2023-10-21 16:25:07 -0300

    Implement first activity and string

--- app/src/main/java/com/azevedogabriel/appestudo_salvatore/MainActivity.kt
+++ app/src/main/java/com/azevedogabriel/appestudo_salvatore/MainActivity.kt
@@ -2,10 +2,21 @@ package com.azevedogabriel.appestudo_salvatore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSend = findViewById<Button>(R.id.buttonOne)
+        val helloWorld = findViewById<TextView>(R.id.helloWorld)
+        val mailSend = findViewById<EditText>(R.id.edtMail)
+
+        buttonSend.setOnClickListener {
+            helloWorld.text = mailSend.text
+        }
     }
 }
